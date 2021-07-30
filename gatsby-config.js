83e02c40e203174242ca524659a62bb87ad2359d
Matheus Bitarães <matheusbitaraesdenovaes@gmail.com>
2021-07-30 16:02:13 -0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "G-SD1VQCT15P",
+        trackingId: "G-K8PPSYJNSP",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: true,
         // Setting this parameter is optional
