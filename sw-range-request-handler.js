6d09e4e42b5ca2d18caf1db3ff04cff723d9730e
matheusbitaraes <matheusbitaraesdenovaes@gmail.com>
2021-08-02 23:04:50 -0300

    video test

--- sw-range-request-handler.js
+++ sw-range-request-handler.js
@@ -1,9 +1,18 @@
-import { registerRoute } from "workbox-routing";
-import { CacheFirst } from "workbox-strategies";
-import { RangeRequestsPlugin } from "workbox-range-requests";
+// range-request-handler.js
 
+// Define workbox globally
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js"
+);
+
+// Bring in workbox libs
+const { registerRoute } = require("workbox-routing");
+const { CacheFirst } = require("workbox-strategies");
+const { RangeRequestsPlugin } = require("workbox-range-requests"); // The fix
+
+// Add Range Request support to fetching videos from cache
 registerRoute(
-  ({ url }) => url.pathname.endsWith(".mp4"),
+  /(\.webm$|\.mp4$)/,
   new CacheFirst({
     plugins: [new RangeRequestsPlugin()],
   })
